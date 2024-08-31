9c942cb9f48623da22d22a4aa3416643da58ea08
Rishi <rishiekandala@gmail.com>
2024-08-31 17:30:05 +0530

    title and content fixed

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -33,8 +33,8 @@ export const Blogs = () => {
        {blogs.map (blog => <BlogCard
        id={blog.id}
         authorName={blog.author.name || "Anonymous" }
-        title={"blog.title"}
-        content={"blog.content"}
+        title={blog.title}
+        content={blog.content}
         publishedDate={"2nd Feb 2024"}
     />)}
      
